3b108ae8ded12c654a8d4f0485b2d7dd3746255d
Shahnur Isgandarli <sisgandarli@gmail.com>
2019-07-26 17:44:45 +0200

    Added initial test file

---
+++ cpp/t_log2hist.cc
@@ -0,0 +1,14 @@
+#include "log2hist.cc"
+#include <gtest/gtest.h>
+ 
+TEST(a, b) { 
+    ASSERT_EQ(6, 6);
+    ASSERT_EQ(18.0, 18.0);
+    ASSERT_EQ(25.4, 25.4);
+    ASSERT_EQ(0, 0);
+}
+ 
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
