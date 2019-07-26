2428cc838b222ac6ed756adf082a80446063b6d0
Shahnur Isgandarli <sisgandarli@gmail.com>
2019-07-26 19:49:30 +0200

    Added some tests

--- cpp/t_log2hist.cc
+++ cpp/t_log2hist.cc
@@ -1,13 +1,61 @@
 #include "log2hist.cc"
 #include <gtest/gtest.h>
  
-TEST(a, b) { 
-    ASSERT_EQ(6, 6);
-    ASSERT_EQ(18.0, 18.0);
-    ASSERT_EQ(25.4, 25.4);
-    ASSERT_EQ(0, 0);
+
+TEST(BoundaryValueTest, 2Bin_Empty) { 
+    Log2Hist log2hist(2);
+    log2hist.Add(10);
+    log2hist.Add(11);
+    log2hist.Add(11.5);
+    
+    uint32_t *bins = log2hist.GetBins();
+    uint32_t res[2] = {0, 0};
+    for (int i = 0; i < 2; i++)
+    {
+        EXPECT_EQ(res[i], bins[i]);
+    }   
+}
+
+TEST(BoundaryValueTest, 2Bins) { 
+    Log2Hist log2hist(2);
+    log2hist.Add(0);
+    log2hist.Add(1);
+    log2hist.Add(1.5);
+    log2hist.Add(2);
+    log2hist.Add(3);
+    log2hist.Add(4);
+    
+    uint32_t *bins = log2hist.GetBins();
+    uint32_t res[2] = {3, 2};
+    for (int i = 0; i < 2; i++)
+    {
+        EXPECT_EQ(res[i], bins[i]);
+    }   
 }
  
+TEST(BoundaryValueTest, 3Bins) { 
+    Log2Hist log2hist(3);
+    log2hist.Add(0);
+    log2hist.Add(0);
+    log2hist.Add(1);
+    log2hist.Add(1.5);
+    log2hist.Add(1.99);
+    log2hist.Add(2);
+    log2hist.Add(3);
+    log2hist.Add(4);
+    log2hist.Add(5);
+    log2hist.Add(5);
+    log2hist.Add(7);
+    log2hist.Add(8);
+    
+    uint32_t *bins = log2hist.GetBins();
+    uint32_t res[3] = {5, 2, 4};
+    for (int i = 0; i < 3; i++)
+    {
+        EXPECT_EQ(res[i], bins[i]);
+    }   
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
