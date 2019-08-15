28aa120c04127611564384c228d62e9c58ca6679
Shahnur Isgandarli <sisgandarli@gmail.com>
2019-08-16 00:09:22 +0200

    added comments to the benchmark script

--- benchmark_run.sh
+++ benchmark_run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# erase all cached data.
 sudo cvmfs_config wipecache
+# try to make connection to cvmfs repository
 cvmfs_config probe
+# run singularity container with sample python code
 singularity exec /cvmfs/unpacked.cern.ch/registry.hub.docker.com/atlasml/ml-base:centos-py-3.6.8 python3 -c "import numpy as np;import tensorflow as tf"
+# run cvmfs_talk command to retrieve information about performace metrics
 sudo cvmfs_talk -i unpacked.cern.ch internal affairs
